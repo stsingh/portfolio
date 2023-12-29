a4d781e6b9b2c26fe3eb759ed81c55f4d99dc02b
stsingh <sahej@gmail.com>
2023-12-29 02:53:55 -0500

    simple navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div className="links">
+                <Link to="/" className="absolute left-6 top-3 flex text-3xl">
+                    Sahej Singh
+                </Link>
+                <ul className="flex absolute right-6 top-3">
+                    <li className='mr-4'>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/experience">Experience</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
